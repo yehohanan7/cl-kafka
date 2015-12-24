0d4611ffe2b14c58792357953fab20e5150afd65
John <john.pradeep@klarna.com>
2015-12-25 00:25:52 +0100

    initial working stream

--- src/cl-kafka.lisp
+++ src/cl-kafka.lisp
@@ -1,26 +1,31 @@
 (in-package #:cl-kafka)
 
 (defun write-bytes (value n stream)
-  (let ((value (ldb (byte n 0) value)))
-    (loop for i from (/ n 8) downto 1
-          do
-             (write-byte (ldb (byte 8 (* (1- i) 8)) value) stream))))
+  (loop for i from (/ n 8) downto 1
+        do  (write-byte (ldb (byte 8 (* (1- i) 8)) value) stream)))
 
 (defun zk-connect (host port)
   (let* ((socket (usocket:socket-connect host port :element-type '(unsigned-byte 8)))
-         (stream (usocket:socket-stream socket))
+         (socket-stream (usocket:socket-stream socket))
          (protocol-version 0)
          (last-zxid 0)
          (timeout 10000)
          (session-id 0)
-         (password (flexi-streams:string-to-octets (make-string 16 :initial-element (code-char 0)))))
+         (password (flexi-streams:string-to-octets (make-string 16 :initial-element (code-char 0))))
+         (ims (flexi-streams:make-in-memory-output-stream))
+         (stream (flexi-streams:make-flexi-stream ims)))
     (write-bytes protocol-version 32 stream)
     (write-bytes last-zxid 64 stream)
     (write-bytes timeout 32 stream)
     (write-bytes session-id 64 stream)
+    (write-bytes (length password) 32 stream)
     (write-sequence password stream)
     (force-output stream)
-    (read-byte stream)))
+    (let ((bytes (flexi-streams:get-output-stream-sequence ims)))
+      (write-bytes (length bytes) 32 socket-stream)
+      (write-sequence bytes socket-stream)
+      (force-output socket-stream)
+      (read-byte socket-stream))))
 
 (zk-connect "localhost" 2181)
 
