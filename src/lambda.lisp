39c84df786974d73984c02ef6158434710bb636b
John <john.pradeep@klarna.com>
2016-01-14 20:41:42 +0100

    add defmacro/g! and defmacro!

--- src/lambda.lisp
+++ src/lambda.lisp
@@ -9,4 +9,35 @@
 (defun equalp* (first &rest rest)
   (every #'(lambda (x) (equalp first x)) rest))
 
+(defun g!-symbol-p (s)
+  (and (symbolp s)
+       (> (length (symbol-name s)) 2)
+       (string= (symbol-name s)
+                "G!"
+                :start1 0
+                :end1 2)))
 
+(defun o!-symbol-p (s)
+  (and (symbolp s)
+       (> (length (symbol-name s)) 2)
+       (string= (symbol-name s)
+                "O!"
+                :start1 0
+                :end1 2)))
+
+(defun o!-symbol-to-g!-symbol (s)
+  (symb "G!" (subseq (symbol-name s) 2)))
+
+
+(defmacro defmacro/g! (name args &rest body)
+  (let ((syms (remove-duplicates (remove-if-not #'g!-symbol-p (flatten body)))))
+    `(defmacro ,name ,args
+       (let ,(mapcar (lambda (s) `(,s (gensym ,(subseq (symbol-name s) 2)))) syms)
+         ,@body))))
+
+(defmacro defmacro! (name args &rest body)
+  (let* ((os (remove-if-not #'o!-symbol-p args))
+         (gs (mapcar #'o!-symbol-to-g!-symbol os)))
+    `(defmacro/g! ,name ,args
+       `(let ,(mapcar #'list (list ,@gs) (list ,@os))
+          ,(progn ,@body)))))
