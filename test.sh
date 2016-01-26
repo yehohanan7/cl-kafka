ba590a70c3eb43bf289c82589bba328e6ce1673f
John <john.pradeep@klarna.com>
2016-01-26 17:31:56 +0100

    add option to run test

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-docker stop kafka && docker rm kafka
-docker run -d -p 2181:2181 -p 9092:9092 --name kafka kafka9
-sleep 10
-sbcl --load run-tests.lisp
+
+case "$1" in
+    run)
+        sbcl --load run-tests.lisp
+        ;;
+    *)
+        docker stop kafka && docker rm kafka
+        docker run -d -p 2181:2181 -p 9092:9092 --name kafka kafka9
+        sleep 10
+        sbcl --load run-tests.lisp
+esac
+
